efc027378f0e852d76cdda83453f18e889e5977d
SergeyVityazev <37889567+SergeyVityazev@users.noreply.github.com>
2018-07-06 12:55:10 +1000

    Update index.php

--- index.php
+++ index.php
@@ -1,42 +1,77 @@
-
 <?php
-// ПОЧТИ ГОТОВА ЗА ВОСКРЕСЕНЬЕ ДОПИЛЮЮЮЮЮЮЮЮЮЮ НЕ СТАВЬТЕ НЕ ЗАЧЕТ
-
 require ("config.php");
-$sql_select="select * from ListBisiness";
+$sql_select="select * from Listbisiness";
 
+if (!empty($_POST['description'])){
+    /*$rows = $connect->exec("INSERT INTO `Listbisiness` VALUES
+    (null,$_POST[description],null)
+    ");
+    $error_array=$connect->errorInfo();
+    echo "Ошибка ". $error_array[2];*/
+    $stmt = $connect->prepare("INSERT INTO `Listbisiness` (id,name, date, status) VALUES (?, ?, ?, ?)");
+    $stmt->bindParam(1, $id);
+    $stmt->bindParam(2, $name);
+    $stmt->bindParam(3, $date);
+    $stmt->bindParam(4, $status);
+    $name=(string)$_POST['description'];
+    $status='Не начинал';
+    $stmt->execute();
+}
 
-echo $_GET['action'];
-
-if (empty($_GET['status']))
-    $_GET['status']='Не начинал';
-
+foreach($connect->query($sql_select) as $row) {
+    if($_GET['action']=='edit.'.$row['id']) {
+        $stmt = $connect->prepare("UPDATE `Listbisiness` SET status='Правится' WHERE id=$row[id]");
+        $stmt->execute();
+    }
+    if($_GET['action']=='done.'.$row['id']) {
+        $stmt = $connect->prepare("UPDATE `Listbisiness` SET status='В процессе' WHERE id=$row[id]");
+        $stmt->execute();
+    }
+    if($_GET['action']=='end.'.$row['id']) {
+        $stmt = $connect->prepare("UPDATE `Listbisiness` SET status='Завершено' WHERE id=$row[id]");
+        $stmt->execute();
+    }
+    if($_GET['action']=='delete.'.$row['id']) {
+        $stmt = $connect->prepare("DELETE FROM `Listbisiness`  WHERE id=$row[id]");
+        $stmt->execute();
+    }
+}
 ?>
 
+<h1>Список дел на сегодня</h1>
 <table>
-    <?php foreach($connect->query($sql_select) as $row){?>
-        <tr>
+    <tr>
+        <td>Номер задачи</td>
+        <td>Описание задачи</td>
+        <td>Время создания задачи</td>
+        <td>Статус</td>
+        <td>Действие</td>
 
+    </tr>
+    <?php foreach($connect->query($sql_select) as $row){
+        ?>
+        <tr>
             <td><?=$row['id']?></td>
             <td><?=$row['name']?></td>
             <td><?=$row['date']?></td>
-            <td><label><?=$_GET['status']?></label></td>
+            <td><?=$row['status']?></td>
             <td>
-                <a href="index.php?action=edit">Изменить</a>
-                <a href="index.php?action=done">Выполнить</a>
-                <a href="index.php?action=delete">Удалить</a>
+                <a href="index.php?action=edit.<?= $row['id']?>">Править</a>
+                <a href="index.php?action=done.<?= $row['id']?>">Начать</a>
+                <a href="index.php?action=end.<?= $row['id']?>">Завершено</a>
+                <a href="index.php?action=delete.<?= $row['id']?>">Удалить</a>
             </td>
         </tr>
     <?php } ?>
 </table>
 
 
-
-
-
-
-
-
+<div style="float: left">
+    <form method="POST">
+        <input type="text" name="description" placeholder="Описание задачи" value="" />
+        <input type="submit" name="save" value="Добавить" />
+    </form>
+</div>
 
 
 <style>
@@ -55,72 +90,4 @@ if (empty($_GET['status']))
     }
 </style>
 
-<h1>Список дел на сегодня</h1>
-<div style="float: left">
-    <form method="POST">
-        <input type="text" name="description" placeholder="Описание задачи" value="" />
-        <input type="submit" name="save" value="Добавить" />
-    </form>
-</div>
-
-<div style="float: left; margin-left: 20px;">
-    <form method="POST">
-        <label for="sort">Сортировать по:</label>
-        <select name="sort_by">
-            <option value="date_created">Дате добавления</option>
-            <option value="is_done">Статусу</option>
-            <option value="description">Описанию</option>
-        </select>
-        <input type="submit" name="sort" value="Отсортировать" />
-    </form>
-</div>
 <div style="clear: both"></div>
-
-<table>
-    <tr>
-        <th>Описание задачи</th>
-        <th>Дата добавления</th>
-        <th>Статус</th>
-        <th></th>
-    </tr>
-    <tr>
-        <td>test</td>
-        <td>2018-06-26 22:03:04</td>
-        <td><span style='color: green;'>Выполнено</span></td>
-        <td>
-            <a href='?id=670&action=edit'>Изменить</a>
-            <a href='?id=670&action=done'>Выполнить</a>
-            <a href='?id=670&action=delete'>Удалить</a>
-        </td>
-    </tr>
-    <tr>
-        <td>Резервное пирование 14.07.2015 04:04</td>
-        <td>2018-06-29 03:45:01</td>
-        <td><span style='color: green;'>Выполнено</span></td>
-        <td>
-            <a href='?id=671&action=edit'>Изменить</a>
-            <a href='?id=671&action=done'>Выполнить</a>
-            <a href='?id=671&action=delete'>Удалить</a>
-        </td>
-    </tr>
-    <tr>
-        <td>Fork from https://</td>
-        <td>2018-06-29 03:47:09</td>
-        <td><span style='color: orange;'>В процессе</span></td>
-        <td>
-            <a href='?id=673&action=edit'>Изменить</a>
-            <a href='?id=673&action=done'>Выполнить</a>
-            <a href='?id=673&action=delete'>Удалить</a>
-        </td>
-    </tr>
-    <tr>
-        <td>test</td>
-        <td>2018-06-29 20:21:05</td>
-        <td><span style='color: orange;'>В процессе</span></td>
-        <td>
-            <a href='?id=674&action=edit'>Изменить</a>
-            <a href='?id=674&action=done'>Выполнить</a>
-            <a href='?id=674&action=delete'>Удалить</a>
-        </td>
-    </tr>
-</table>
